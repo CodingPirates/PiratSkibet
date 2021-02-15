a6e95006e6e441085af0a7fba2a52f24e28a042b
Mateusz <mg@morningtrain.dk>
2021-02-15 13:02:25 +0100

    rearranged file and added "highlighted" scope

--- app/Models/Content/Video.php
+++ app/Models/Content/Video.php
@@ -3,6 +3,7 @@
 namespace App\Models\Content;
 
 use App\Support\Traits\Changeable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
@@ -19,25 +20,18 @@ class Video extends Model
         'embed_link',
     ];
 
-    private static function extractVideoId(string $id): string
+    /////////////////////////////
+    /// Scopes
+    /////////////////////////////
+
+    public function scopeHighlighted(Builder $q, bool $highlighted = true): Builder
     {
-        // Only type === YouTube
-        $parts = parse_url($id);
-
-        if (isset($parts['query'])) {
-            parse_str($parts['query'], $query);
-
-            if (isset($query['v'])) {
-                return $query['v'];
-            }
-        }
-
-        if (isset($parts['path'])) {
-            return Str::afterLast($parts['path'], '/');
-        }
-
-        return $id;
+        return $q->where('is_highlighted', $highlighted);
     }
+
+    /////////////////////////////
+    /// Getters
+    /////////////////////////////
 
     public function getEmbedLinkAttribute(): string
     {
@@ -59,4 +53,30 @@ class Video extends Model
 
         return "https://youtu.be/{$videoId}";
     }
+
+
+    /////////////////////////////
+    /// Helpers
+    /////////////////////////////
+
+    private static function extractVideoId(string $id): string
+    {
+        // Only type === YouTube
+        $parts = parse_url($id);
+
+        if (isset($parts['query'])) {
+            parse_str($parts['query'], $query);
+
+            if (isset($query['v'])) {
+                return $query['v'];
+            }
+        }
+
+        if (isset($parts['path'])) {
+            return Str::afterLast($parts['path'], '/');
+        }
+
+        return $id;
+    }
+
 }
