80dce6d33dc18375aebf0b7ec8a36db0d245124a
Mateusz <mg@morningtrain.dk>
2021-02-15 14:28:01 +0100

    wrapped content in Paper component, to provide a background

--- resources/js/widgets/tv/VideoGrid.js
+++ resources/js/widgets/tv/VideoGrid.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Widget from 'widgets/Widget'
+import Paper from 'layouts/Paper'
 import Section from 'layouts/Section'
 import { Collection } from '@morningtrain/react-resources'
 import * as Filters from 'support/filters'
@@ -9,32 +10,39 @@ export default class VideoGrid extends Widget {
 
   renderWidget () {
     return (
-      <React.Fragment>
+      <Section>
+        <Paper size={'small'}>
 
-        <Section className='youtube-video-section' header={<h2>Nyeste Videoer</h2>}>
-          <Collection resourceName='content.videos' operationContext={'newest'}>
-            <Filters.Static constraint='$per_page' value={3} />
-            <VideoGridLoop/>
-          </Collection>
-        </Section>
+          <Section className='youtube-video-section'
+                   header={<h2>Nyeste Videoer</h2>}>
+            <Collection resourceName='content.videos'
+                        operationContext={'newest'}>
+              <Filters.Static constraint='$per_page' value={3}/>
+              <VideoGridLoop/>
+            </Collection>
+          </Section>
 
-        <Section className='youtube-video-section' header={<h2>Highlighted</h2>}>
-          <Collection resourceName='content.videos' operationContext={'highlighted'}>
-            <Filters.Static constraint='highlighted' value={true} />
-            <Filters.Static constraint='$per_page' value={3} />
-            <VideoGridLoop/>
-          </Collection>
-        </Section>
+          <Section className='youtube-video-section'
+                   header={<h2>Highlighted</h2>}>
+            <Collection resourceName='content.videos'
+                        operationContext={'highlighted'}>
+              <Filters.Static constraint='highlighted' value={true}/>
+              <Filters.Static constraint='$per_page' value={3}/>
+              <VideoGridLoop/>
+            </Collection>
+          </Section>
 
-        <Section className='youtube-video-section' header={<h2>Alle videoer</h2>}>
-          <Collection resourceName='content.videos' operationContext={'all'}>
-            <Filters.Static constraint='$per_page' value={3} />
-            <VideoGridLoop/>
-            <Filters.Pagination/>
-          </Collection>
-        </Section>
+          <Section className='youtube-video-section'
+                   header={<h2>Alle videoer</h2>}>
+            <Collection resourceName='content.videos' operationContext={'all'}>
+              <Filters.Static constraint='$per_page' value={3}/>
+              <VideoGridLoop/>
+              <Filters.Pagination/>
+            </Collection>
+          </Section>
 
-      </React.Fragment>
+        </Paper>
+      </Section>
     )
   }
 }
