57a792af764a059b884a7c5a3ff0722988c75e32
Mateusz <mg@morningtrain.dk>
2021-02-24 11:01:03 +0100

    Youtube component, can now display channel livestreams

--- resources/js/support/displays/YoutubeVideo.js
+++ resources/js/support/displays/YoutubeVideo.js
@@ -1,23 +1,35 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { inject } from '@morningtrain/react-decorators'
 import Display from '@morningtrain/react-displays/Display'
 
 @inject(['model'])
 export default class YoutubeVideo extends Display {
 
-  constructor () {
-    super()
+  constructor (props) {
+    super(props)
 
     this.state = {
-      loaded: false,
+      // When embedding a youtube channel livestream link
+      // We don't have the specific video id
+      // And therefore can't preload the image
+      loaded: props.is_channel,
     }
+  }
+
+  get src() {
+    if (this.props.is_channel) {
+      return `https://www.youtube.com/embed/live_stream?channel=${this.value}`;
+    }
+
+    return `https://www.youtube.com/embed/${this.value}?autoplay=1&rel=0`;
   }
 
   renderIframe () {
     return (
       <div className="youtube-player">
         <iframe
-          src={`https://www.youtube.com/embed/${this.value}?autoplay=1&rel=0`}
+          src={this.src}
           frameBorder='0'
           allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
           allowFullScreen
@@ -41,4 +53,12 @@ export default class YoutubeVideo extends Display {
   render () {
     return this.state.loaded ? this.renderIframe() : this.renderPreview()
   }
+}
+
+YoutubeVideo.defaultProps = {
+  is_channel: false,
+}
+
+YoutubeVideo.propTypes = {
+  is_channel: PropTypes.bool,
 }
