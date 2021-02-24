62ccad9c7b18759c764cb77a07ea37616a9a256b
Mateusz <mg@morningtrain.dk>
2021-02-24 09:40:42 +0100

    ensure only 1 livestream is live at a time

--- app/Models/Content/Livestream.php
+++ app/Models/Content/Livestream.php
@@ -9,4 +9,15 @@ class Livestream extends Model
     protected $casts = [
         'is_live'  => 'boolean',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::saving(function (Livestream $livestream) {
+            if ($livestream->is_live && $livestream->isDirty('is_live')) {
+                static::query()->update(['is_live' => false]);
+            }
+        });
+    }
 }
