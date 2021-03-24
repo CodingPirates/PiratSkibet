ae276fbe2d5bef625ab710a733f452d8701867f0
Mateusz <mg@morningtrain.dk>
2021-03-24 09:53:30 +0100

    disabled "New in forum" functionality

--- resources/js/widgets/animations/islands/SkeletonIsland.js
+++ resources/js/widgets/animations/islands/SkeletonIsland.js
@@ -2,10 +2,6 @@ import React from 'react'
 import Animation from 'widgets/animations/Animation'
 import Island from 'widgets/animations/partials/Island'
 import SkeletonHead from 'widgets/animations/partials/SkeletonHead'
-import Link from 'widgets/navigation/Link'
-import { Collection } from '@morningtrain/react-resources'
-import TimedIterator from 'helpers/timedIterator/TimedIterator'
-import * as Displays from 'support/displays'
 
 export default class SkeletonIsland extends Animation {
   static get defaultProps () {
@@ -36,16 +32,7 @@ export default class SkeletonIsland extends Animation {
     return (
       <div className='skeleton-island'>
         <SkeletonHead left='0' top='4px' zIndex='2' />
-        <Island islandType='black' left='0' top='0' zIndex='1'>
-          <p className='chat-heading'>Nyt i Piratsnak:</p>
-          <Collection resourceName='forum.thread' operationName='newest'>
-            <TimedIterator duration={1} interval={5000}>
-              <Link className='chat-title' route='app.forum.thread' parameters={{ thread: 'model:id' }}>
-                <Displays.Text name='subject' />
-              </Link>
-            </TimedIterator>
-          </Collection>
-        </Island>
+        <Island islandType='black' left='0' top='0' zIndex='1'/>
       </div>
     )
   }
