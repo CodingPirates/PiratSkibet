7912d48903d911d6596495c3c547b0033e9230c7
Simon Jønsson <sj@morningtrain.dk>
2021-09-20 15:01:45 +0200

    ✨ Add ResourceLink model & migration

---
+++ database/migrations/2021_09_20_125833_create_resource_links_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateResourceLinksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('resource_links', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('url');
+            $table->string('text');
+            $table->bigInteger('category_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('resource_links');
+    }
+}
