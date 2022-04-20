14d1408f46c58b845516d8b91f3f2feb84ac5382
Simon Jønsson <sj@morningtrain.dk>
2022-04-20 10:13:13 +0200

    🔧 Setup config for storage

---
+++ app/Console/Commands/SyncFilesToOtherDisk.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+use MorningTrain\Laravel\Fields\Files\Models\File;
+
+class SyncFilesToOtherDisk extends Command
+{
+    protected $signature = 'files:sync-disk';
+
+    protected $description = 'Command for syncing all files to specific disk';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $newDisk = 's3';
+
+        File::all()
+            ->each(function(File $file) use ($newDisk) {
+                if(!$file->file_exists) {
+                    return;
+                }
+
+                $stored = Storage::disk($newDisk)->put($file->path, $file->content);
+
+                if($stored) {
+                    $file->disk = $newDisk;
+                    $file->save();
+                }
+            });
+    }
+}
